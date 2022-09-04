c32cabcf18dc07e7df543a94b21146a7cc72e16e
unknown <oguzhandemiraslan@gmail.com>
2022-09-04 15:46:58 -0400

    validation updated

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -17,7 +23,9 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
     setEnteredNameTouched(true);
+
     if (!enteredNameIsValid) {
       return;
     }
@@ -49,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
